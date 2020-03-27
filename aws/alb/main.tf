e0e11edf5ea07a8aa4ebbe04e78d17fd80611d70
Choi Yeongjun <frozenmoai@gmail.com>
2020-03-27 15:47:06 +0900

    Added a treat_msising_data attribute at tg alarm

--- aws/alb/main.tf
+++ aws/alb/main.tf
@@ -47,6 +47,7 @@ locals {
       threshold          = lookup(target_group.http5xx_alarm, "threshold", 0)
       period             = lookup(target_group.http5xx_alarm, "period", 300)
       evaluation_periods = lookup(target_group.http5xx_alarm, "evaluation_periods", 1)
+      treat_missing_data = lookup(target_group.http5xx_alarm, "treat_missing_data", "notBreaching")
     } if lookup(lookup(target_group, "http5xx_alarm", {}), "enabled", true)
   }
 }
@@ -304,6 +305,7 @@ resource "aws_cloudwatch_metric_alarm" "http5xx" {
   threshold          = each.value.threshold
   period             = each.value.period
   evaluation_periods = each.value.evaluation_periods
+  treat_missing_data = each.value.treat_missing_data
 
   dimensions = {
     LoadBalancer = aws_alb.this.arn_suffix
